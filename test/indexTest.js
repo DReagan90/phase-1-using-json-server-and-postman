d51b879c0fcab26a7540a286b23aaed0135fde34
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-03-24 00:07:28 +0300

    done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,17 @@
 // The code below ensures that students who are using CodeGrade will get credit
 // for the code-along in Canvas; you can disregard it.
+// Importing the Chai assertion library
+const { expect } = require('chai');
 
-require("./helpers.js");
-
-describe("", () => {
-  describe("", () => {
-    it("Test passing", () => {
-      return true;
+// Define the test suite
+describe('Test Suite', () => {
+  // Define a test group (you can have nested test groups)
+  describe('Test Group', () => {
+    // Define the test case
+    it('should pass the test', () => {
+      // A simple assertion that will pass
+      expect(true).to.be.true; // This test will pass because true is indeed true
     });
   });
 });
+
